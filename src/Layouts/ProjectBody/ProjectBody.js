43c77f55d77449090add226faaab7c174421be41
Samarendra Gouda <samarendragouda@gmail.com>
2022-12-26 22:43:32 +0530

    Projects section done

--- src/Layouts/ProjectBody/ProjectBody.js
+++ src/Layouts/ProjectBody/ProjectBody.js
@@ -2,18 +2,22 @@ import React from 'react';
 import styles from './ProjectBody.module.css';
 import Header from '../../Components/Header/Header';
 import ProjectCard from '../../Components/ProjectCard/ProjectCard';
+import List from './Project_List';
 // import Button from '../../Components/Button/Button';
 
 
 const ProjectBody = (props) => {
 
+    var list = List.Project_List;
     return (
         <div className={styles.Body}>
             <Header />
             <div className={styles.container}>
-                <ProjectCard />
-                <ProjectCard />
-                <ProjectCard />
+                {list.map((m,index) => {
+                    return(
+                        <ProjectCard heading={m.heading} label={m.label} desc={m.desc} link={m.link} index={index} key={m.sl}/>
+                    );
+                })}
             </div>
         </div>
     );
