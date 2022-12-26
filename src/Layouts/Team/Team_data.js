e61bd66b051e54d1b7dd2a1e36a23094daabc7de
Samarendra Gouda <samarendragouda@gmail.com>
2022-12-26 15:54:08 +0530

    Teams Card socials added

---
+++ src/Layouts/Team/Team_data.js
@@ -0,0 +1,26 @@
+var joint_secy = [
+    {
+        name: "Pradyuman Verma",
+        designation: "Joint Secretary",
+        twitter:"",
+        linkedin:"",
+    },
+    {
+        name: "Vaibhav Khanna",
+        designation: "Joint Secretary",
+        twitter:"",
+        linkedin:"",
+    },
+    {
+        name: "Samarpan Dutta",
+        designation: "Joint Secretary",
+        twitter:"",
+        linkedin:"",
+    },
+    {
+        name: "Aditya Gite",
+        designation: "Joint Secretary",
+        twitter:"",
+        linkedin:"",
+    }
+]
