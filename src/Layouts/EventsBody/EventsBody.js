c2506bfdaabe332320ee760b6d4ae4583d1847f7
Samarendra Gouda <samarendragouda@gmail.com>
2022-12-26 23:06:06 +0530

    Events Section Done

--- src/Layouts/EventsBody/EventsBody.js
+++ src/Layouts/EventsBody/EventsBody.js
@@ -2,19 +2,22 @@ import React from 'react';
 import styles from './EventBody.module.css';
 import Header from '../../Components/Header/Header';
 import EventCard from '../../Components/EventCard/EventCard';
+import List from "./Events_list";
 
 
 
 
 const EventsBody = (props) => {
-
+    const list = List.Event_list;
     return (
         <div className={styles.Body}>
             <Header />
             <div className={styles.container}>
-                <EventCard />
-                <EventCard />
-                <EventCard />
+                {list.map((m,index)=>{
+                    return(
+                        <EventCard name={m.eventName} date={m.date} venue={m.venue} desc={m.desc} link={m.link} index={index} key={m.eventName} />
+                    );
+                })}
             </div>
         </div>
     );
--- src/Layouts/EventsBody/Events_list.js
+++ src/Layouts/EventsBody/Events_list.js
@@ -1,9 +1,25 @@
 var Event_list = [
     {
-        eventName:"",
-        date:"",
-        venue:"",
-        desc:"",
+        eventName:"Event 1",
+        date:"13 September 2022",
+        venue:"Room no. 104, New LHC",
+        desc:"Here we should write details of the event like what it is about and any necessary details.",
+        link:""
+    },
+    {
+        eventName:"Event 2",
+        date:"13 September 2022",
+        venue:"Room no. 104, New LHC",
+        desc:"Here we should write details of the event like what it is about and any necessary details.",
+        link:""
+    },
+    {
+        eventName:"Event 3",
+        date:"13 September 2022",
+        venue:"Room no. 104, New LHC",
+        desc:"Here we should write details of the event like what it is about and any necessary details.",
         link:""
     }
 ]
+
+export default {Event_list};
