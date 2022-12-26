eee23fb4dd98e0a983ab648c15d61f165f98c2cf
Samarendra Gouda <samarendragouda@gmail.com>
2022-12-26 16:39:18 +0530

    other pages started

--- src/Components/EventCard/EventCard.js
+++ src/Components/EventCard/EventCard.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import styles from './EventCard.module.css'
+import Button from '../Button/Button';
 
 
 const EventCard = (props) => {
 
     return (
-        <div className={styles.container}>
-            <img />
-            <div className={styles.content}>
-                
-            </div>
+      <div className={styles.container}>
+        <div className={styles.img}></div>
+        <div className={styles.content}>
+          <h3>Event Name</h3>
+          <div className={styles.label}>
+            <p>13 September 2022 <br /> Room No. 104, New LHC </p>
+            
+          </div>
+          <p className={styles.desc}>
+            Here we should write details of the event like what it is about and any necessary details.
+          </p>
+          <Button str={"View Recording"} />
         </div>
+      </div>
     );
 };
 
--- src/Components/ProjectCard/ProjectCard.js
+++ src/Components/ProjectCard/ProjectCard.js
@@ -1,15 +1,19 @@
 import React from "react";
 import styles from "./ProjectCard.module.css";
 import Label from "../Label/Label"
+import Button from "../Button/Button";
 
 const ProjectCard = (props) => {
   return (
     <div className={styles.container}>
-      <img />
+      <div className={styles.img}></div>
       <div className={styles.content}>
+        <h3>Project Heading</h3>
         <div className={styles.label}>
           <Label str={"Project Label"} />
         </div>
+        <p className={styles.desc}>Here we have to write description of the project, the prizes it won if build at some hackathon.</p>
+        <Button str={"View Project"} />
       </div>
     </div>
   );
