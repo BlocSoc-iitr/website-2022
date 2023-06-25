75a242d5b0abb41e2038d925aa6b469ebbe8e226
Mihir <mihir6453@gmail.com>
2023-06-25 22:44:06 +0530

    Added latest events

--- src/Layouts/EventsBody/Events_list.js
+++ src/Layouts/EventsBody/Events_list.js
@@ -5,6 +5,46 @@ import sob from './events/sob.png';
 
 var Event_list = [
     {
+        eventName:"Opportunities with Web3 & ICP",
+        date:"10th May 2023",
+        venue:"L2-103, New LHC",
+        desc:"BlocSoc, in collaboration with Crewsphere, conducted this session to share the opportunities with Web3 and the Inter-Computer Protocol (ICP) blockchain with students. The speakers from Crewsphere shared details about upcoming grants and mentorship",
+        link:"",
+        // image:,
+    },
+    {
+        eventName:"Scaling Ethereum With Cryptography",
+        date:"12th April 2023",
+        venue:"L2-103, New LHC",
+        desc:"This lecture covered the fundamentals of cryptography and various cryptographic algorithms and how it is used in blockchain. We also discussed the basic idea of zero knowledge proofs.",
+        link:"",
+        // image:,
+    },
+    {
+        eventName:"Azure Dev Day",
+        date:"22nd January 2023",
+        venue:"L2-103, New LHC",
+        desc:"This workshop aimed to share the knowledge of Web3, Blockchain and Azure among the students. Speakers from Microsoft Azure interacted with students and gave them insights about these emerging technology",
+        link:"",
+        // image:,
+    },
+    {
+        eventName:"Introduction to Smart Contracts",
+        date:"18th January 2023",
+        venue:"",
+        desc:"A workshop conducted by BlocSoc for students trying to explore WEB3, here we dived into the fundamentals of the language Solidity and how to write basic smart contracts using it.",
+        link:"",
+        // image:,
+    },
+    {
+        eventName:"Bitcoin For India X BlocSoc",
+        date:"16th January 2023 to 20th February 2023",
+        venue:"",
+        desc:"BlocSoc in collaboration with Bitcoin For India conducted a 5 week workshop. The workshop covered Saylor Academy’s Course PRDV 151: Bitcoin For Everybody, with weekly sessions for doubts, discussions and further insight about the course.",
+        link:"",
+        // image:,
+    },
+    {
         eventName:"WAGMI x BlocSoc IITR",
         date:"27th December 2022 \t 19:00 IST",
         venue:"Zoom",
